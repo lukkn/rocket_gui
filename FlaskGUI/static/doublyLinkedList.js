2237dc96d08d0387e27c8af95eeb0b93c2df1178
lukkn <70594133+lukkn@users.noreply.github.com>
2024-01-09 09:32:24 -0500

    created doubly linked list class

---
+++ FlaskGUI/static/doublyLinkedList.js
@@ -0,0 +1,51 @@
+class CircularBuffer {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.head = null; 
+        this.tail = null; 
+    }
+
+    popFirst(){
+        this.head = this.head.next;
+        this.head.previous = null;
+        this.size --;
+    }
+
+    popLast(){
+        this.tail = this.tail.previous;
+        this.tail.next = null;
+        this.size--;
+    }
+
+    push(value){
+        const newNode = {
+            value: value,
+            next: null,
+            previous: this.tail,
+        }
+
+        // if the list is full, remove the oldest element
+        if (this.size >= this.capacity){
+            this.popFirst();
+        }
+
+        // if the list is empty, set new node as head. If not, set the next node of the previous tail to new node.
+        if (this.head == null){
+            this.head = newNode;
+        } else {
+            this.tail.next = newNode;
+        }
+    
+        this.tail = newNode;
+        this.size ++;
+    }   
+}
+
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+        this.previous = null;
+    }
+}
