d6753a09011a7ba364487337264bed066a85ed49
Remus287 <79165136+Remus287@users.noreply.github.com>
2024-01-11 16:23:16 -0500

    removed unused imports

--- FlaskGUI/app.py
+++ FlaskGUI/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 from flask_socketio import SocketIO
 from threading import Lock
 import json
