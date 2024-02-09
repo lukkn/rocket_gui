51fbe0e3eb49eeabe72522216b9b0550dddfc943
Remus287 <79165136+Remus287@users.noreply.github.com>
2024-02-09 18:17:28 -0500

    removed files listed in .gitignore

--- motesoft/Client_GUI/shell_scripts/mac_gtk_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#install homebrew if the user doesn't have it yet
-brew -h || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-#install required dependencies for MOTESOFT
-brew install pygobject3 gtk+3
--- motesoft/legacy_testing/Example_Code/int_cast_test.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    int32_t x0 = -1;
-    uint32_t x1 = x0;
-    std::cout << x1;
-    return 0;
-}
