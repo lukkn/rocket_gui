d41804ff9e1d088c0bc0b01187039c3447335e39
lukkn <70594133+lukkn@users.noreply.github.com>
2024-03-07 14:08:32 -0500

    Fixed sensor graph sizing bug

--- static/makeChart.js
+++ static/makeChart.js
@@ -25,6 +25,8 @@ function plotLines(chartNumber) {
     const context = canvas.getContext('2d');
     const activeSensors = activeSensorsList[containerID];
 
+    resizeCanvasToDisplaySize(canvas)
+    
     // clear canvas
     context.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -171,3 +173,19 @@ function handleGraphCheckbox(containerID, checkboxID, sensor){
         }
     }
 }
+
+function resizeCanvasToDisplaySize(canvas) {
+    // Lookup the size the browser is displaying the canvas in CSS pixels.
+    const displayWidth  = canvas.clientWidth;
+    const displayHeight = canvas.clientHeight;
+    
+    // Check if the canvas is not the same size.
+    const needResize = canvas.width  !== displayWidth ||
+                        canvas.height !== displayHeight;
+    if (needResize) {
+        // Make the canvas the same size
+        canvas.width  = displayWidth;
+        canvas.height = displayHeight;
+    }
+}
+
